8fe19a19bd96c3b471de3e5cd0fbabb672516f93
Rafat J. Meraz <razhossain201@gmail.com>
2020-07-26 19:50:29 +0600

    change password ui added

---
+++ lib/src/view/ui/change_password.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/src/view/utils/constants.dart';
+import 'package:news_app/src/view/utils/reuseable_widgets.dart';
+
+class ChangePassword extends StatefulWidget {
+  @override
+  _ChangePasswordState createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  final TextEditingController _userNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  BackButton(),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                    child: Text('Change Password', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 0.6)),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.verified_user,
+                  size: 100,
+                  color: kDarkBlueColor,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text('example@gmail.com', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300, letterSpacing: 0.6)),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Card(
+                elevation: 2,
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: <Widget>[
+                      InputTextField(
+                          forgotOption: false,
+                          forgotFunction: null,
+                          textInputType: TextInputType.text,
+                          hint: 'Current Password',
+                          obscureText: false,
+                          textEditingController: _userNameController,
+                          iconData: Icons.person
+                      ),
+                      SizedBox(height: 10,),
+                      InputTextField(
+                          forgotOption: false,
+                          forgotFunction: null,
+                          textInputType: TextInputType.text,
+                          hint: 'New Password',
+                          obscureText: false,
+                          textEditingController: _userNameController,
+                          iconData: Icons.person
+                      ),
+                      SizedBox(height: 15,),
+                      RoundedButton(
+                        inProgress: false,
+                        onPressed: (){
+
+                        },
+                        buttonText: 'Save Changes',
+                        disable: false,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        )
+    );
+  }
+}
--- lib/src/view/ui/profile.dart
+++ lib/src/view/ui/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/src/view/ui/change_password.dart';
 import 'package:news_app/src/view/ui/change_username.dart';
 import 'package:news_app/src/view/utils/constants.dart';
 
@@ -66,7 +67,7 @@ class _ProfileState extends State<Profile> {
                     title: Text('Change Password', style: TextStyle(fontWeight: FontWeight.w500)),
                     leading: Icon(Icons.lock, color: kBlackColor),
                     onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => ChangeUsername()));
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ChangePassword()));
                     },
                   ),
                 ],
